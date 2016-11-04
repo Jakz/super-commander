a4914e10c170cdb72056ee0ee9d175f743f54ca4
Jack <jack@pixbits.com>
2016-11-04 06:17:53 +0100

    added ConsolePanel

--- src/main/java/com/pixbits/supercmd/App.java
+++ src/main/java/com/pixbits/supercmd/App.java
@@ -1,13 +1,23 @@
 package com.pixbits.supercmd;
 
+import javax.swing.JFrame;
+
+import com.pixbits.supercmd.ui.ConsolePanel;
+import com.pixbits.supercmd.ui.UIUtils;
+import com.pixbits.supercmd.ui.WrapperFrame;
+
 /**
  * Hello world!
  *
  */
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+  public static void main( String[] args )
+  {
+    WrapperFrame<ConsolePanel> frame = UIUtils.buildFrame(new ConsolePanel(25, 80), "Console");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
 }
